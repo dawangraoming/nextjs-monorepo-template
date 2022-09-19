51bef4ae53084df526043966d4ad821b85390670
dawangraoming <dawangraoming@hotmail.com>
2022-09-19 17:05:29 +0800

    chore(apps): :wrench: support for compiling modules in packages

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,7 +1,8 @@
 const withPlugins = require('next-compose-plugins');
+const withTM = require('next-transpile-modules')(['@dawangraoming/uikit']);
 
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 });
 
-module.exports = withPlugins([[withBundleAnalyzer]], {});
+module.exports = withPlugins([[withBundleAnalyzer], [withTM]], {});
